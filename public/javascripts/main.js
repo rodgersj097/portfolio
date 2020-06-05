7953676ba247c0f938a1943899c00da9ed4d793e
rodgersj097 <200320013@student.georgianc.on.ca>
2020-06-05 12:44:08 -0400

    Styling for links

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,8 +2,17 @@
  var fullName = document.querySelector('#fullName')
  var email = document.querySelector('#email')
  var content = document.querySelector('#content')
+var workItem = document.querySelectorAll('.workItem')
 
+workItem.forEach(item => {
 
+item.addEventListener('click', function(){
+
+    var url = this.dataset.src;
+    var win = window.open(url, '_blank'); 
+    win.focus()
+})
+});
  submit.addEventListener('click', function() {
      var validatedEmail = validateEmail(email.value)
      var data = {
@@ -24,3 +33,4 @@
      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
      return re.test(String(email).toLowerCase());
  }
+
